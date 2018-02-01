17280e1d6b836a7844f334c71d7fcb833c18d00e
aing <aingaran2003@gmail.com>
2018-01-31 23:28:36 -0800

    added comments

--- 8006a1.sh
+++ 8006a1.sh
@@ -39,18 +39,16 @@ iptables -A OUTPUT -m tcp -p tcp --dport 443 -j PERMIT-TRAFFIC
 iptables -A INPUT -p all -j ALL-TRAFFIC
 iptables -A OUTPUT -p all -j ALL-TRAFFIC
 
-#RULE#########################################################################
-#in order for DNS to work properly
+#RULES#########################################################################
 #iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 #drop inboud port 80 (http) from source port <1024
-iptables -A PERMIT-TRAFFIC -p tcp -m tcp --dport 80 --sport :1023 -j DROP #NOT WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+iptables -A PERMIT-TRAFFIC -p tcp -m tcp --dport 80 --sport :1023 -j DROP 
 
 #drop all incoming SYN packets
 iptables -A INPUT -m tcp -p tcp --syn -j DROP 
-#iptables -A INPUT -p tcp --tcp-flags ALL SYN -j DROP
 
-
+#Reject all packets for ports in the reject list
 IFS=',' read -ra LIST <<< "$REJECT"
 for i in "${LIST[@]}"; do
     iptables -A ALL-TRAFFIC -p tcp --sport $i -j DROP
@@ -59,6 +57,7 @@ for i in "${LIST[@]}"; do
     iptables -A ALL-TRAFFIC -p udp --dport $i -j DROP
 done
 
+#Accept all packets for ports in the permitted tcp list
 IFS=',' read -ra LIST <<< "$PERMIT_TCP"
 for i in "${LIST[@]}"; do
     iptables -A PERMIT-TRAFFIC -p tcp --dport $i -j ACCEPT
@@ -67,6 +66,7 @@ for i in "${LIST[@]}"; do
     iptables -A ALL-TRAFFIC -p tcp --sport $i -j ACCEPT
 done
 
+#Accept all packets for ports in the permitted udp list
 IFS=',' read -ra LIST <<< "$PERMIT_UDP"
 for i in "${LIST[@]}"; do
     iptables -A PERMIT-TRAFFIC -p udp --dport $i -j ACCEPT
